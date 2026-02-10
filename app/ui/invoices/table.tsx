8ed9f36658a511d51a1be2bc4681e9ad47d6c245
AnnaBova19 <annabova17@gmail.com>
2026-02-10 18:21:14 +0200

    Added search functionality and pagination to Invoices page

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,6 +11,7 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  // It is a Server Component that fetches its own data, so you can pass the searchParams prop from the page to the component.
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
