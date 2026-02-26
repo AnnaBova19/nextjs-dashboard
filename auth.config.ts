e1e30781275c2b003f4d7e92a753999f103f72ae
AnnaBova19 <annabova17@gmail.com>
2026-02-26 15:22:10 +0200

    Updated auth.config.ts to allow public assets

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,13 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
+      const isPublicAsset = 
+        nextUrl.pathname === '/robots.txt' || 
+        nextUrl.pathname === '/sitemap.xml';
+
+      if (isPublicAsset) return true; 
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
