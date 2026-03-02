b0b37bb3da529b00207ed9ac6e8d53fc51b61242
AnnaBova19 <annabova17@gmail.com>
2026-03-02 23:29:18 +0200

    Added placeholder to tip tap editor

--- app/ui/shared/rich-text-editor.tsx
+++ app/ui/shared/rich-text-editor.tsx
@@ -4,19 +4,26 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { EditorToolbar } from "./tip-tap-toolbar";
-import { cn } from "@/lib/utils";
+import Placeholder from "@tiptap/extension-placeholder";
 
 export function RichEditor({
   value = '',
+  placeholder = '',
   invalid,
   onChange
 }: {
   value?: string;
+  placeholder?: string;
   invalid?: boolean;
   onChange?: (content: string) => void;
 }) {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      Placeholder.configure({
+        placeholder,
+      }),
+    ],
     content: value,
     immediatelyRender: false,
     onUpdate: ({ editor }) => {
@@ -29,8 +36,13 @@ export function RichEditor({
       <EditorToolbar editor={editor} />
       <EditorContent
         editor={editor}
-        className="prose dark:prose-invert max-w-none px-3 py-1 min-h-[200px] focus-within:outline-none text-[14px]
-          [&_.ProseMirror]:outline-none [&_.ProseMirror]:focus:outline-none"
+        className="prose dark:prose-invert max-w-none px-3 py-2 min-h-[200px] focus-within:outline-none text-[14px]
+          [&_.ProseMirror]:outline-none [&_.ProseMirror]:focus:outline-none
+          [&_.ProseMirror_p.is-editor-empty:first-child::before]:content-[attr(data-placeholder)]
+          [&_.ProseMirror_p.is-editor-empty:first-child::before]:text-muted-foreground
+          [&_.ProseMirror_p.is-editor-empty:first-child::before]:float-left
+          [&_.ProseMirror_p.is-editor-empty:first-child::before]:pointer-events-none
+          [&_.ProseMirror_p.is-editor-empty:first-child::before]:h-0"
       />
     </div>
   );
